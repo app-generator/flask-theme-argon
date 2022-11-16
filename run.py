2acc394cdd65238f19e13853e9171daef7c1bfa6
mominur-helios <mominur.rahman@heliosinsider.com>
2022-11-16 16:20:18 +0600

    v0.0.1 release

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 from argon_app import app
+from flask_minify import Minify
+
+DEBUG  = app.config['DEBUG']
+
+if not DEBUG:
+    Minify(app=app, html=True, js=False, cssless=False)
 
 if __name__ == "__main__":
     app.run()
